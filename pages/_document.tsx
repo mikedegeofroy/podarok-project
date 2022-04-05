1b8f8964d16243c1bfc6bc4bc58935f6eacd847f
Mike de Geofroy <59496187+MikeDeGeofroy@users.noreply.github.com>
2022-04-05 14:46:56 +0300

    added GA (limited)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,23 @@ export default function Document() {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Roboto:wght@100;300;400&display=swap" rel="stylesheet"/>
+                <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Roboto:wght@100;300;400&display=swap" rel="stylesheet" />
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `,
+                    }}
+                />
             </Head>
             <body>
                 <Main />
